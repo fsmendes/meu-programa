f76ae2be2913d6fd36e6641de09ca397873ccb08
Fabio dos Santos Mendes <fsmendes@gmail.com>
2020-05-17 16:07:24 -0300

    Added main program script file.

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------
+# Description:
+# Author: Fabio dos Santos Mendes <fsmendes@gmail.com>
+# Created at: Sun May 17 16:05:35 -03 2020
+# Computer: package
+# System: Linux 4.19.0-9-amd64 on x86_64
+#
+# Copyright (c) 2020 Fabio dos Santos Mendes -  All rights reserved.
+#
+#----------------------------------------------------------------------
+# Configure section:
+
+#!/bin/bash
+
+echo "Hello World!"
